33ba7fbd3dc9089865b778726e2fd8a8878ec011
Janam-Ke <janamouko@gmail.com>
2023-08-22 09:08:58 +0300

    added templates

--- myportfolio/urls.py
+++ myportfolio/urls.py
@@ -20,6 +20,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('myprojects/', include('myprojects.urls')),
-    path("blog/", include("blog.urls")),
+    path('', include('myprojects.urls')),
+    path("", include("blog.urls")),
 ]
